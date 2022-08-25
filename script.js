ae41b503a696bc01468147eecdfff24c78fe9e96
NchiaEmmanuela <ekeinchiaemmanuela@gmail.com>
2022-08-25 19:43:03 +0100

    manupulating css

--- script.js
+++ script.js
@@ -10,15 +10,31 @@ document.querySelector('.check').addEventListener('click', () => {
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No message';
+
   } else if (guess === secreteNumber) {
     document.querySelector('.message').textContent = 'Correct number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secreteNumber;
+
   } else if (guess > secreteNumber) {
-    document.querySelector('.message').textContent = 'guess is too high';
-    score--
-    document.querySelector('.score').textContent = score
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'guess is too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'you lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+    
   } else if (guess < secreteNumber) {
-    document.querySelector('.message').textContent = 'guess is too low';
-    score--
-    document.querySelector('.score').textContent = score
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'guess is too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'you lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
